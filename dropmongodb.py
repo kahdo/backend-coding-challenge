25df44ba1dd4c8ba5e266deb074567f4a4f99325
Cadu VboxLF <cadu.coelho@gmail.com>
2017-01-31 20:03:33 -0200

    Cleaned up some files.

--- dropmongodb.py
+++ dropmongodb.py
@@ -2,12 +2,6 @@
 
 from ubcmhn.bootstrap import config
 from ubcmhn.model import DbEngine
-
-from hackernews import HackerNews
-
-# task test
-from ubcmhn.celeryapp.tasks.debug.tasks import hello
-
 # Raw DB
 print("Dropping DB")
 db = DbEngine(config)
